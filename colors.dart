bb3321c135081605e2bee89153abd92dbea2331a
manikuttan-davud <112249810+manikuttan-davud@users.noreply.github.com>
2022-11-09 11:52:45 +0530

    Add files via upload

---
+++ colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+const Color colorFAAA14 = Color(0xFFFAAA14);
+const Color colorEEAB40 = Color(0x10EEAB40);
+const Color colorEE5F40 = Color(0x50EEAB40);
+const Color colorFF6262 = Color(0xFFFF6262);
+const Color colorFFFFFF= Color(0xFFFFFFFF);
+const Color colorF7F7F7= Color(0xFFF7F7F7);
